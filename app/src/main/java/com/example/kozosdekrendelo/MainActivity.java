e76f4f10fe61ff8d7e7ac8950bcbccbd4dabff99
Peter <peterkertesz@gmail.com>
2021-06-20 23:05:21 +0200

    proba github 3

--- app/src/main/java/com/example/kozosdekrendelo/MainActivity.java
+++ app/src/main/java/com/example/kozosdekrendelo/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-// proba 2021.06.20
+// proba 2021.06.20 c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
