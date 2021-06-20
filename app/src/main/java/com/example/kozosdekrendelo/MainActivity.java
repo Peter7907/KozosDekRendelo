d773f14abe6303241ca7fb36b9dfd4c03bc731e1
Peter <peterkertesz@gmail.com>
2021-06-20 23:01:53 +0200

    proba github 3

---
+++ app/src/main/java/com/example/kozosdekrendelo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.kozosdekrendelo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+// proba 2021.06.20
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
